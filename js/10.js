29e085f7efb54428fc41647651cc159ef31b205c
avkotau <kotov.alexander1@mail.ru>
2023-01-06 14:18:01 +0100

    tasks "06.01.23"

---
+++ js/10.js
@@ -0,0 +1,12 @@
+var summation = function (num) {
+    let result = 0;
+    for (let i = 0; i <= num; i++) {
+        result += i
+    }
+    console.log(result)
+}
+summation(8)
+
+// 1 === 1;
+// 2 === 3;
+// 8 === 36
