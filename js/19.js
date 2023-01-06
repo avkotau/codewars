b15647e01a2120e9109fb58905817074b6db9afe
avkotau <kotov.alexander1@mail.ru>
2023-01-06 19:26:38 +0100

    tasks "06.01.23"

--- js/19.js
+++ js/19.js
@@ -1,7 +1,17 @@
+// function getDifference(arr) {
+//     console.log(arr.map(i => i <= 0 ? i*-1 : -i))
+// }
+//
+// let arr = [1,-2,3,-4,5];
+//
+// getDifference(arr)
+
+//or
+
 function getDifference(arr) {
-    console.log(arr.map(i => i <= 0 ? i*-1 : -i))
+    console.log(arr.map(i => -i))
 }
 
-let arr = [1,-2,3,-4,5];
+let arr = [1,-2,3,-4,5, -10];
 
 getDifference(arr)
