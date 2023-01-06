03d1792332cb62b7bdd4a623bac310ec49f6636d
avkotau <kotov.alexander1@mail.ru>
2023-01-06 18:20:07 +0100

    tasks "06.01.23"

--- js/17.js
+++ js/17.js
@@ -10,3 +10,17 @@ function basicOp(operation, value1, value2) {
     }
 }
 basicOp('+', 4, 7)
+// or
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case "+" :
+            return value1 + value2
+        case "-":
+            return value1 - value2
+        case "*":
+            return value1 * value2
+        default:
+            return value1 / value2
+    }
+}
+basicOp('+', 4, 7)
