8a1704646a1950caa635cba561b81650f2efd104
avkotau <kotov.alexander1@mail.ru>
2023-01-09 08:34:29 +0100

    tasks "09.01.23"

---
+++ js/42.js
@@ -0,0 +1,18 @@
+function rowSumOddNumbers(n) {
+
+    console.log(n**3)
+
+}
+rowSumOddNumbers(42)
+//42, 74088
+
+//           1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+
+// result
+// 1 -->  1
+// 2 --> 3 + 5 = 8
