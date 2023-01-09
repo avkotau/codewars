1f5a14b48f425cf5d4a24175d6f11a2c740a8425
avkotau <kotov.alexander1@mail.ru>
2023-01-09 11:38:16 +0100

    tasks "09.01.23"

---
+++ js/45.js
@@ -0,0 +1,12 @@
+function solution(str){
+
+     console.log(str.split('').reverse().join(''))
+
+}
+
+solution('world')
+
+//('world'), 'dlrow');
+//     solution('hello'), 'olleh';
+//     solution(''), '';
+//     solution('h'), 'h'
