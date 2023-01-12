a4891ce71d816546e8dc5c9769c972e11c496f82
avkotau <kotov.alexander1@mail.ru>
2023-01-12 10:32:13 +0100

    tasks "12.01.23"

---
+++ js/54.js
@@ -0,0 +1,11 @@
+function abbrevName(name){
+
+
+    console.log(name.split(' ').map(n => n.charAt(0).toUpperCase() + '.').join(' '))
+
+
+}
+abbrevName("sam Harris") //"S.H"
+
+//str.substring(indexA[, indexB])
+// massiv[0].charAt(1)
