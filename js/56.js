5ccbad52a69cd1908cf7ffaa045f370a47d6b8ab
avkotau <kotov.alexander1@mail.ru>
2023-01-14 11:20:03 +0100

    tasks "14.01.23"

--- js/56.js
+++ js/56.js
@@ -1,6 +1,13 @@
 function finalGrade (exam, projects) {
-    return exam > 90 && projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0
+    console.log( exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0)
 }
 
+    finalGrade(0, 11) //0
+    finalGrade(32, 12)// 0;
+    finalGrade(52, 11)// 75;
 //    finalGrade(100, 12)// 100;
 //     finalGrade(85, 5)// 90;
+
+//exam = 0, projects = 11: expected +0 to equal 100
+// Testing for 100 random tests
+// Testing for exam = 32, projects = 12: expected +0 to equal 100
