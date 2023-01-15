de4e76d8562727933ab104b89b107831d513141f
avkotau <kotov.alexander1@mail.ru>
2023-01-15 16:32:35 +0100

    tasks "15.01.23"

---
+++ js/60.js
@@ -0,0 +1,32 @@
+let languageAll = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}
+function greet(language) {
+    for(let i = 0; i < Object.keys(languageAll).length; i++) {
+        if(languageAll.hasOwnProperty(language)){
+            return `${languageAll[language]}`
+        } else return `Welcome`
+    }
+
+}
+
+greet('english')
+
+
+
