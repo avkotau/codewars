8ad499af87a2c0ab067509c711e2a1b8c1dac014
avkotau <kotov.alexander1@mail.ru>
2023-01-16 16:12:45 +0100

    tasks "16.01.23"

--- js/61.js
+++ js/61.js
@@ -1,8 +1,12 @@
-function enough(cap, on, wait) {
-    let result = cap - on - wait;
-    return result >= 0 ? 0 : Math.abs(result)
-}
+// function enough(cap, on, wait) {
+//     let result = cap - on - wait;
+//     return result >= 0 ? 0 : Math.abs(result)
+// }
 
+//or
+function enough(cap, on, wait) {
+    console.log(Math.max(wait + on - cap, 0));
+}
 
  enough(10, 5, 5)//, 0);
  enough(100, 60, 50)//, 10);
