9c51352478a58885ed154af3ddc5b36b353d7438
avkotau <kotov.alexander1@mail.ru>
2023-01-16 16:13:26 +0100

    tasks "16.01.23"

---
+++ js/62.js
@@ -0,0 +1,13 @@
+function removeChar(str){
+    return str.slice(1,-1)
+
+};
+
+removeChar('eloquent')
+
+
+//assert.strictEqual(removeChar('eloquent'), 'loquen');
+//     assert.strictEqual(removeChar('country'), 'ountr');
+//     assert.strictEqual(removeChar('person'), 'erso');
+//     assert.strictEqual(removeChar('place'), 'lac');
+//     assert.strictEqual(removeChar('ooopsss'), 'oopss')
