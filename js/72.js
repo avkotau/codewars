5c4775254c32e50bd72f178ce26dac4b119fb6c1
avkotau <kotov.alexander1@mail.ru>
2023-01-19 17:33:55 +0100

    tasks "19.01.23"

---
+++ js/72.js
@@ -0,0 +1,12 @@
+let checkForFactor = (base, factor) => base % factor === 0
+
+
+checkForFactor(10,2) // true)
+checkForFactor(63,7) // true)
+checkForFactor(2450,5) // true)
+checkForFactor(24612,3) // true)
+
+checkForFactor(9,2) // false)
+checkForFactor(653,7) // false)
+checkForFactor(2453,5) // false)
+checkForFactor(24617,3) // false)
