56c2ff4d5e89a47efcd1cf215eb23d180a161d1a
avkotau <kotov.alexander1@mail.ru>
2023-01-20 18:54:28 +0100

    tasks "20.01.23"

---
+++ js/74.js
@@ -0,0 +1,10 @@
+function reverseWords(str) {
+    let arrLetters = str.split('');
+    let reverseLetters = arrLetters.reverse().join('');
+    let result = reverseLetters.split(' ').reverse().join(' ')
+}
+
+reverseWords('The quick brown fox jumps over the lazy dog.') //, 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+// reverseWords('apple') //, 'elppa');
+// reverseWords('a b c d') //, 'a b c d');
+// reverseWords('double  spaced  words') //, 'elbuod  decaps  sdrow');
