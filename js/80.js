44d48027dbc950140b1bc6d6ff537735492a3456
avkotau <kotov.alexander1@mail.ru>
2023-01-22 14:29:27 +0100

    tasks "22.01.23"

---
+++ js/80.js
@@ -0,0 +1,7 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        console.log(length * width * height)
+    }
+}
+Kata.getVolumeOfCuboid(1,2,2) //,  4);
+Kata.getVolumeOfCuboid(6,2,5) //, 60);
