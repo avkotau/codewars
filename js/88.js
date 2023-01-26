c895436cb1a3f8d86d1e216d42f437fa0add0a46
avkotau <kotov.alexander1@mail.ru>
2023-01-26 16:07:48 +0100

    tasks "26.01.23"

---
+++ js/88.js
@@ -0,0 +1,15 @@
+function countBy(x, n) {
+    let sum = x*n
+    let z = [];
+    for (let i = 1; i <= sum; i++) {
+        if (i % x === 0) {
+            z.push(i)
+        }
+    }
+    console.log(z);
+}
+
+
+
+countBy(1,10), [1,2,3,4,5,6,7,8,9,10] //, "Array does not match")
+countBy(2,5), [2,4,6,8,10] //, "Array does not match")
