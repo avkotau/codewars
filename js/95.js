8b093961dafa27f7846f3d33b353d2d19be2bab7
avkotau <kotov.alexander1@mail.ru>
2023-01-29 09:46:13 +0100

    tasks "29.01.23"

--- js/95.js
+++ js/95.js
@@ -3,7 +3,14 @@ let capitals = function (word) {
     word.split([]).map((n, i) => n === n.toUpperCase() ? resultArr.push(i) : '')
     console.log(resultArr)
 };
-
+//or
+// let capitals = function (word) {
+//   return word.split('').reduce(function(n, l, i){
+//     return /[A-Z]/.test(l) && n.push(i), n;
+//   }, []);
+// };
+//or
+// let capitals = word => [...word].reduce((arr, v, i) => /[A-Z]/.test(v)? [...arr, i] : arr, []);
 
 capitals('CodEWaRs') //, [0,3,4,6] )
 
