62f605d78e83ae3216084f7de01d81bba2cda98c
avkotau <kotov.alexander1@mail.ru>
2023-02-01 12:21:35 +0100

    tasks "01.02.23"

---
+++ js/107.js
@@ -0,0 +1,9 @@
+function hexToDec(hexString){
+   console.log(parseInt(hexString,16))
+}
+
+hexToDec("1") //, 1);
+hexToDec("a") //, 10);
+hexToDec("10") //, 16);
+hexToDec("FF") //, 255);
+hexToDec("-C") //, -12);
