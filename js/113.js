7bb3bb196cc9ef6f26f172b6199e95b83d1e1414
avkotau <kotov.alexander1@mail.ru>
2023-02-03 10:58:38 +0100

    tasks "03.02.23"

---
+++ js/113.js
@@ -0,0 +1,17 @@
+function getChar(c){
+    console.log(String.fromCharCode(c))
+}
+
+
+
+getChar(55) // '7')
+getChar(56) // '8')
+getChar(57) // '9')
+getChar(58) // ':')
+getChar(59) // ';')
+getChar(60) // '<')
+getChar(61) // '=')
+getChar(62) // '>')
+getChar(63) // '?')
+getChar(64) // '@')
+getChar(65) // 'A')
