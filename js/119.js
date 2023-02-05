57a97866a8351b01945291657fce6c1e6bc0de87
avkotau <kotov.alexander1@mail.ru>
2023-02-05 13:37:11 +0100

    tasks "05.02.23"

---
+++ js/119.js
@@ -0,0 +1,7 @@
+function nameShuffler(str){
+    console.log(str.split(' ').reverse().join(' '))
+}
+
+nameShuffler('john McClane') //,'McClane john');
+nameShuffler('Mary jeggins') //,'jeggins Mary')
+nameShuffler('tom jerry') //,'jerry tom')
