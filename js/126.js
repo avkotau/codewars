74fdfa3c681a9234d7f934c91f9c9771c74ca246
avkotau <kotov.alexander1@mail.ru>
2023-02-07 17:50:07 +0100

    tasks "07.02.23"

---
+++ js/126.js
@@ -0,0 +1,6 @@
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = Number(laLigaGoals + championsLeagueGoals + copaDelReyGoals)
+console.log(laLigaGoals,  championsLeagueGoals,  copaDelReyGoals)
