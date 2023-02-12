919d304b340f557e037c2411e3a8de2e45de8184
avkotau <kotov.alexander1@mail.ru>
2023-02-12 14:21:38 +0100

    tasks "12.02.23"

--- js/138.js
+++ js/138.js
@@ -7,6 +7,13 @@ function addLength(str) {
      console.log(result)
 }
 
+//or
+//function addLength(str){
+//   return str.split(" ").map(s => `${s} ${s.length}`)
+// }
+//or
+//let addLength = (str) => str.split(' ').map(n => `${n} ${n.length}`)
+
 addLength('apple ban');//["apple 5", "ban 3"]);
 
 addLength('you will win')//,["you 3", "will 4", "win 3"]));
