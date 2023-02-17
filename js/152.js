d9fcb1a35db80ac4a83de4a4ca5f43395127cdef
avkotau <kotov.alexander1@mail.ru>
2023-02-17 08:57:29 +0100

    tasks "17.02.23"

--- js/152.js
+++ js/152.js
@@ -1,7 +1,16 @@
 function filter_list(l) {
     let result = []
-    console.log(l.filter(n => typeof n === 'number' ? result.push(n) : '' ))
+    //console.log(l.filter(n => typeof n === 'number' ? result.push(n) : '' ))
+    console.log(l.filter(n => typeof n === 'number' ? n : 0 ))
 }
+// or
+//function filter_list(l) {
+//  return l.filter(v => typeof v == "number")
+// }
+//or
+//function filter_list(l) {
+//   return l.filter(function(v) {return typeof v == 'number'})
+// }
 
 filter_list([1,2,'a','b']) //,[1,2], 'For input [1,2,"a","b"]');
 filter_list([1,'a','b',0,15]) //,[1,0,15], 'For input [1,"a","b",0,15]');
