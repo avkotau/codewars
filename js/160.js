ee47c09942d563a1e04c6ef486801b9a77ea7e02
avkotau <kotov.alexander1@mail.ru>
2023-02-26 09:00:36 +0100

    tasks "26.02.23"

---
+++ js/160.js
@@ -0,0 +1,8 @@
+function remainder(n, m){
+    console.log( n > m ? n % m : m % n)
+}
+
+remainder(17,5) //, 2, 'Returned value should be the value left over after dividing as much as possible. For input n = 17, m = 5');
+remainder(13, 72) //, remainder(72, 13), 'The order the arguments are passed should not matter');
+remainder(1, 0) //, 'Divide by zero should return NaN');
+remainder(0, 0) //, 'Divide by zero should return NaN');
