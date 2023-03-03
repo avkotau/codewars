95bd070ea3e3631d771389789fa129f0953fb38e
avkotau <kotov.alexander1@mail.ru>
2023-03-03 17:19:49 +0100

    tasks "03.03.23"

--- js/175.js
+++ js/175.js
@@ -1,5 +1,9 @@
-function include(arr, item) {
-    console.log(arr.some(a => a === item))
+// function include(arr, item) {
+//     console.log(arr.some(a => a === item))
+// }
+//or
+function include(arr, item){
+  return arr.includes(item);
 }
 include([1,2,3,4], 3); //, true )
 include([1,2,4,5], 3); //, false)
