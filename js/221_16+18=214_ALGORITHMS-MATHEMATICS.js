59279122e7f5fdd96f07a5d311f01bf64e607199
avkotau <kotov.alexander1@mail.ru>
2023-04-28 16:45:47 +0200

    tasks "28.04.23"

--- js/221_16+18=214_ALGORITHMS-MATHEMATICS.js
+++ js/221_16+18=214_ALGORITHMS-MATHEMATICS.js
@@ -1,22 +1,36 @@
 function add(num1, num2) {
-    let arr1 = num1 + ''.split()
-    let arr2 = num2 + ''.split()
+    let result = [];
+    let num1str = String(num1)
+    let num2str = String(num2)
+    let sum = Number(num1str[num1str.length-1]) + Number(num2str[num2str.length-1]);
+    result.unshift(sum);
 
-    let result = []
-    for (let i = arr2.length-1; i >= 0; i--) {
-        // console.log('fun;')
-        // console.log(+num1.toString()[i] + +num2.toString()[i]);
-        result.unshift(+num1.toString()[i] + +num2.toString()[i])
+    let i = num1str.length - 2;
+    let j = num2str.length - 2;
+    while (i >= 0 && j >= 0) {
+        sum = Number(num1str[i]) + Number(num2str[j]);
+        result.unshift(sum);
+        i--;
+        j--;
     }
 
-    // console.log(Number(result.join('')))
-    console.log(result.join(''))
+    while (i >= 0) {
+        result.unshift(Number(num1str[i]));
+        i--;
+    }
+
+    while (j >= 0) {
+        result.unshift(Number(num2str[j]));
+        j--;
+    }
+
+    return Number(result.join(''));
 }
 
 add(2, 11) // 13);
-// add(0, 1) // 1);
-// add(0, 0) // 0);
+add(0, 1) // 1);
+add(0, 0) // 0);
 
-// add(16, 18) // 214);
-// add(26, 39) // 515);
-// add(122, 81) // 1103);
+add(16, 18) // 214);
+add(26, 39) // 515);
+console.log(add(122, 81)) // 1103);
