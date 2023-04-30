9aaa716981488dc229ba4a7b4a69d30f1fbc53c3
avkotau <kotov.alexander1@mail.ru>
2023-04-30 07:27:27 +0200

    tasks "30.04.23"

---
+++ js/223_Convert a Boolean to a String_STRINGS-FUNDAMENTALS.js
@@ -0,0 +1,6 @@
+function booleanToString(b){
+    return String(b)
+}
+
+booleanToString(true) // "true", 'When we pass in true, we want the string "true" as output');
+booleanToString(false) // "false", 'When we pass in false, we want the string "false" as output');
