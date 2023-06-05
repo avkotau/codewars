1d1d690cfa87434fd3c79626c9ce63efbb396816
avkotau <kotov.alexander1@mail.ru>
2023-06-05 12:22:37 +0200

    How many arguments (FUNDAMENTALS)

---
+++ js/237.js
@@ -0,0 +1,8 @@
+function args_count (...arg) {
+    return arg.length
+}
+
+console.log(args_count(1, 2)) // 2);
+console.log(args_count()) // 0);
+console.log(args_count('A', 'B', 'C')) // 3);
+console.log(args_count(["foo", "bar"])) // 1);
