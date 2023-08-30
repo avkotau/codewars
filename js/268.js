21be4cab2861e4c87d1fe964d7f636232393778f
avkotau <kotov.alexander1@mail.ru>
2023-08-30 10:48:45 +0200

    fix: add variant 2

--- js/268.js
+++ js/268.js
@@ -1,14 +1,30 @@
+// const flatArray = [1, 2, 3, [4, 5, 6, [7, 8], 9, 10, [11, 12]]]
+// let result = []
+// function foo(flatArray) {
+//     for (let i = 0; i < flatArray.length; i++) {
+//         if (Array.isArray(flatArray[i])) {
+//             foo(flatArray[i])
+//         } else {
+//             result.push(flatArray[i])
+//         }
+//     }
+//     return result
+// }
+//
+// console.log(foo(flatArray))
+
+//variant 2
+
 const flatArray = [1, 2, 3, [4, 5, 6, [7, 8], 9, 10, [11, 12]]]
 let result = []
 function foo(flatArray) {
-    for (let i = 0; i < flatArray.length; i++) {
-        if (Array.isArray(flatArray[i])) {
-            foo(flatArray[i])
+    for (let i of flatArray) {
+        if (Array.isArray(i)) {
+            foo(i)
         } else {
-            result.push(flatArray[i])
+            result.push(i)
         }
     }
     return result
 }
-
 console.log(foo(flatArray))
