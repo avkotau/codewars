4e843621fa64a0ccb58982a21844c8d91a3ce1ab
avkotau <kotov.alexander1@mail.ru>
2023-08-30 10:35:20 +0200

    create from under arrays One array. Fundamentals, Array

---
+++ js/268.js
@@ -0,0 +1,14 @@
+const flatArray = [1, 2, 3, [4, 5, 6, [7, 8], 9, 10, [11, 12]]]
+let result = []
+function foo(flatArray) {
+    for (let i = 0; i < flatArray.length; i++) {
+        if (Array.isArray(flatArray[i])) {
+            foo(flatArray[i])
+        } else {
+            result.push(flatArray[i])
+        }
+    }
+    return result
+}
+
+console.log(foo(flatArray))
