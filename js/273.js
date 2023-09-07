60f9144b48628c6eb1dab0acbe65ddc093b15434
avkotau <kotov.alexander1@mail.ru>
2023-09-07 20:35:46 +0200

    fix: change all functionality for execution speed

--- js/273.js
+++ js/273.js
@@ -1,25 +1,24 @@
 //145
 function count(str) {
-    const array = str.split('')
-    const obj = {}
-    for (let i = 0; i < array.length; i++) {
-        if(!obj.hasOwnProperty(array[i])) {
-            obj[array[i]] = [array[i]]
-        } else {
-            obj[array[i]].push(array[i])
-        }
+    let obj = {};
+
+    // add quantity characters in obj
+    for (let char of str) {
+        obj[char] = (obj[char] || 0) + 1;
     }
-    let longestArray = [];
-    for (const key in obj) {
-        if ( obj[key].length > longestArray.length) {
-            longestArray = obj[key];
-        }
-    }
-    return Array.from(new Set (longestArray))
+
+    // Find the maximum characters
+    let maxFreq = Math.max(...Object.values(obj));
+
+    // Filter the characters that match the maximum quantity
+    let mostFrequentChars = Object.keys(obj).filter(char => obj[char] === maxFreq);
+
+    // If there's only one character with maximum quantity, return it. Otherwise, return the array.
+    return mostFrequentChars.length === 1 ? mostFrequentChars[0] : mostFrequentChars;
 }
 
 console.log(count('abbccc'))
 // 'c'
 
-// console.log(count('abbcccddd'))
+console.log(count('abbcccddd'))
 // ['c', 'd']
