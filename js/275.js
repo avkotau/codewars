260339375d6f6277eb3d1057495fa5ba886d719a
avkotau <kotov.alexander1@mail.ru>
2023-09-11 14:18:43 +0200

    fix: toCamelCase. String

--- js/275.js
+++ js/275.js
@@ -1,6 +1,6 @@
 const toCamelCase = (str) => {
   const arrLowerWords = str.toLowerCase().split(' ')
-    return arrLowerWords.map(w => w[0].toUpperCase() + w.slice(1, w.length)).join(' ')
+    return arrLowerWords.map(w => w[0].toUpperCase() + w.slice(1)).join(' ')
 }
 
 console.log(toCamelCase('To caMel CAsE'))
