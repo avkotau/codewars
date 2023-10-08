71f80d78661129850ec4475a9ea00daf48210750
avkotau <kotov.alexander1@mail.ru>
2023-10-08 18:23:35 +0200

    feat: add additional variants solution function

--- js/286.js
+++ js/286.js
@@ -6,34 +6,33 @@
 // inputs are guaranteed to be valid input like above example, there is no numerical digit in original form.
 
 function uncompress(str) {
-    let numStack = [];
-    let strStack = [];
-    let result = '';
-    let fullSubStr = ''
+    const stack = [];
+    let num = '';
 
-    for (let i = 0; i < str.length; i++) {
-        let tempVariable = ''
-        if (Number.isFinite(Number(str[i]))) {
-            tempVariable += str[i]
-            if (str[i + 1] === '(') {
-                numStack.push(Number(tempVariable))
+    for (const char of str) {
+        if (!isNaN(char)) {
+            // If the character is a number, save it
+            num += char;
+        } else if (char === '(') {
+            // If the character is an open parenthesis, add the number to the stack and clear num
+            stack.push(Number(num));
+            num = '';
+        } else if (char === ')') {
+            // If the character is a closing parenthesis, unpack the contents up to the last opening parenthesis
+            let temp = '';
+            while (typeof stack[stack.length - 1] !== 'number') {
+                temp = stack.pop() + temp;
             }
-        } else if (str[i] === '(') {
-            strStack.push(str[i])
-        } else if (str[i] === ')') {
-            strStack.pop()
-            if (strStack.length === 0) {
-                fullSubStr = result.repeat(numStack[0])
-
-                result = ''
-            }
+            const count = stack.pop();
+            stack.push(temp.repeat(count));
         } else {
-            result += str[i]
+            // Otherwise we just add the symbol to the stack
+            stack.push(char);
         }
     }
-    return fullSubStr
+    return stack.join('');
 }
 
 console.log(uncompress('3(ab)')) // 'ababab'
-// console.log(uncompress('3(ab2(c))')) // 'abccabccabcc'
-// console.log(uncompress('2(BFE1(dev))3(2(lover))')) // 'BFEdevBFEdevloverloverloverloverloverlover'
+console.log(uncompress('3(ab2(c))')) // 'abccabccabcc'
+console.log(uncompress('2(BFE1(dev))3(2(lover))')) // 'BFEdevBFEdevloverloverloverloverloverlover'
